85ab092554028f41aab07fa8141b3796b0f477c6
李清霸 <a1172791203@163.com>
2024-12-01 00:02:03 +0800

    主题更改

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '来加入社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://chat.xiaoheihe.cn/iqru7dt3'
+  HEO_SOCIAL_CARD_URL: 'https://chat.xiaoheihe.cn/iqru7dt3',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
