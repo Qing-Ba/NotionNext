6cb374151315af6a2d455edfe7bd9aa7526de2df
李清霸 <a1172791203@163.com>
2024-12-02 15:52:25 +0800

    修改导航栏布局

--- themes/heo/components/Logo.js
+++ themes/heo/components/Logo.js
@@ -7,7 +7,7 @@ const Logo = props => {
   const { siteInfo } = props
   return (
     <Link href='/' passHref legacyBehavior>
-      <div className='flex flex-nowrap items-center cursor-pointer font-extrabold'>
+      <div className='flex flex-nowrap items-center cursor-pointer font-extrabold min-w-[50rem]'>
         <LazyImage
           src={siteInfo?.icon}
           width={24}
